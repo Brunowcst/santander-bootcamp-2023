305c0f69309217aad9e427110fbd1f3a38050195
Brunowcst <brunoporfirio022@gmail.com>
2024-05-05 14:51:10 -0300

    TIpos de variáveis

---
+++ src/com/santanderbootcamp/bruno/variaveis/TiposDeVariaveis.java
@@ -0,0 +1,11 @@
+package src.com.santanderbootcamp.bruno.variaveis;
+
+public class TiposDeVariaveis {
+    public static void main(String[] args) throws Exception {
+        //adição de final transforma uma variável em constante(Não modificável)
+        final int NUMERO = 15;
+        //escrever constantes em caixa alta(Convenção)
+
+        System.out.println(NUMERO);
+    }
+}
